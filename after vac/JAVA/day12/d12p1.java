66bbc0f6fa653490a46d8bd7dba240777569bc4e
adithya3403 <a.dty.3403@gmail.com>
2023-02-02 11:44:46 +0530

    Create d12p1.java

---
+++ after vac/JAVA/day12/d12p1.java
@@ -0,0 +1,88 @@
+/*
+
+Sri Vihaan Travels announced a family trip to Sri Nagar, for which they began 
+taking reservations. They are only offering 2 and 3-seater tickets for this trip.
+
+Each member of certain groups is identifiable by the group ID to which he or 
+she belongs. Each group is uniquely identified by its group ID.
+
+The travel administration decided that the ticket will only be issued to members of the same group. It is decided that No wasted tickets will be issued 
+(i.e., If a person is alone, then he is not allowed to get a ticket).
+
+You are given a list of integers person_id[], where person_id[i] is the group ID 
+of the ith person in the list. Your objective is to determine whether fewer tickets 
+should be issued to cover everyone in each group. Print -1, if it is not possible.
+
+
+Input Format:
+-------------
+Line-1: Comma separated integers, Person's group-ID
+
+Output Format:
+--------------
+Print an integer result.
+
+
+Sample Input-1:
+---------------
+1,1,3,2,2,2,1,2,2,3
+
+Sample Output-1:
+----------------
+4
+
+Explanation: 
+------------
+They can book the tickets for all the people, as follows:
+- Issue the 3-seater ticket, to group with ID-1 of size 3.
+- Issue the 2-seater ticket, to group with ID-3 of size 2.
+- Issue the 3-seater ticket, to group with ID-2 of size 5.
+- Issue the 2-seater ticket, to group with ID-2 of size 5.
+So, you have issued 4 tickets in total.
+
+
+Sample Input-2:
+---------------
+1,1,1,2,2,3
+
+Sample Output-2:
+----------------
+-1
+
+Explanation: 
+------------
+There is a group with ID-3 of size 1. 
+The administration's ruling states that a single person cannot receive a ticket.
+So, print -1.
+
+*/
+
+import java.util.*;
+
+public class d12p1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] str = sc.nextLine().split(",");
+        int[] arr = new int[str.length];
+        for (int i = 0; i < str.length; i++)
+            arr[i] = Integer.parseInt(str[i]);
+        System.out.println(solve(arr));
+        sc.close();
+    }
+
+    static int solve(int[] arr) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i : arr)
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        int res = 0;
+        for (int i : map.values()) {
+            if (i == 1)
+                return -1;
+            if (i % 3 == 0)
+                res += i / 3;
+            else
+                res += (i / 3) + 1;
+        }
+        return res;
+    }
+}
