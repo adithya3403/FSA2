6bd77074af53c2836ab2b39c83b7a4dde1a2c750
adithya3403 <a.dty.3403@gmail.com>
2023-03-02 20:16:24 +0530

    Create d33p1.java

---
+++ march/JAVA/day33/d33p1.java
@@ -0,0 +1,66 @@
+/*
+
+Given an array of integers nums, calculate the pivot index of this array.
+
+The pivot index is the index where the sum of all the numbers strictly to the left of the index is equal to the sum of all the numbers strictly to the index's right.
+
+If the index is on the left edge of the array, then the left sum is 0 because there are no elements to the left.
+This also applies to the right edge of the array.
+
+Return the leftmost pivot index. If no such index exists, return -1.
+
+
+Example 1:
+input =6
+1 7 3 6 5 6
+output =3
+
+Explanation:
+The pivot index is 3.
+Left sum = nums[0] + nums[1] + nums[2] = 1 + 7 + 3 = 11
+Right sum = nums[4] + nums[5] = 5 + 6 = 11
+Example 2:
+
+Input: nums = [1,2,3]
+Output: -1
+Explanation:
+There is no index that satisfies the conditions in the problem statement.
+
+
+Example 3:
+
+Input: nums = [2,1,-1]
+Output: 0
+Explanation:
+The pivot index is 0.
+Left sum = 0 (no elements to the left of index 0)
+Right sum = nums[1] + nums[2] = 1 + -1 = 0
+
+*/
+
+import java.util.*;
+
+public class d33p1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+        System.out.println(findPivot(arr));
+        sc.close();
+    }
+
+    static int findPivot(int[] arr) {
+        int total = 0, left = 0;
+        for (int i : arr)
+            total += i;
+        for (int i = 0; i < arr.length; i++) {
+            total -= arr[i];
+            if (left == total)
+                return i;
+            left += arr[i];
+        }
+        return -1;
+    }
+}
