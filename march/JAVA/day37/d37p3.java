6671551b1579e9110253181d761599e261e60aac
adithya3403 <a.dty.3403@gmail.com>
2023-03-23 16:27:15 +0530

    Create d37p3.java

---
+++ march/JAVA/day37/d37p3.java
@@ -0,0 +1,50 @@
+/*
+
+Ramu is located at the top-left corner of a  box  (marked 'Entry').
+ramu can only move either down or right at any point in time. 
+ramu is trying to reach the bottom-right corner of the box (marked 'End').
+
+How many possible unique paths are there?
+
+Example 1:
+Input: m = 3, n = 2
+Output: 3
+
+
+Explanation:
+From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+1. Right -> Right -> Down
+2. Right -> Down -> Right
+3. Down -> Right -> Right
+
+Example 2:
+Input: m = 7, n = 3
+Output: 28
+
+*/
+
+import java.util.*;
+
+public class d37p3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        System.out.println(uniquePaths(m, n));
+        sc.close();
+    }
+
+    public static int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++)
+            dp[i][0] = 1;
+        for (int i = 0; i < n; i++)
+            dp[0][i] = 1;
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+}
